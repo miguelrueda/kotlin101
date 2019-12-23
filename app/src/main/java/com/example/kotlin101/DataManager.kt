759fa404d16494c4da822cac6747e5b97ef61910
miguelrueda <miguel.rueda@outlook.com>
2019-12-23 15:31:50 -0600

    Module 4 finished

---
+++ app/src/main/java/com/example/kotlin101/DataManager.kt
@@ -0,0 +1,26 @@
+package com.example.kotlin101
+
+class DataManager {
+
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses() {
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("android_async", "Android Async Programming and Services")
+        courses.set(course.courseId,course)
+
+        course = CourseInfo(title ="Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses.set(course.courseId,course)
+
+        course = CourseInfo("java_core", "Android Async Programming and Services")
+        courses.set(course.courseId,course)
+    }
+
+}
--- app/src/main/java/com/example/kotlin101/NoteKeeperData.kt
+++ app/src/main/java/com/example/kotlin101/NoteKeeperData.kt
@@ -1,5 +1,11 @@
 package com.example.kotlin101
 
-class CourseInfo (val courseId: String, val title: String)
+class CourseInfo (val courseId: String, val title: String) {
+
+    override fun toString(): String {
+        return title
+    }
+
+}
 
 class NoteInfo (var course: CourseInfo, var title: String, var text: String)
